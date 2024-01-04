a8c5cc169a09d8c1be66ebe547a9efe049cff05c
Luis Gonçalves <106309135+tfptmaster@users.noreply.github.com>
2024-01-04 12:12:51 +0100

    initial project

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+import ThemeToogler from './ThemeToogler'
+
+const Header = () => {
+  return (
+    <header>
+        <ThemeToogler />
+    </header>
+  )
+}
+
+export default Header
